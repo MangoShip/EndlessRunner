5d72eae4d4f4909aaa02b7aa5e4479d90622166b
kkang4 <63477132+kkang4@users.noreply.github.com>
2020-04-30 13:25:35 -0700

    tank selection done but needs more

--- src/scenes/TankSelection.js
+++ src/scenes/TankSelection.js
@@ -4,10 +4,17 @@ class TankSelection extends Phaser.Scene{
     }
 
     preload(){
- 
+        // load images/tile sprite
+        this.load.image('T34', './assets/T34.png');
+        this.load.image('SU85', './assets/SU85.png');
+        this.load.image('kv2', './assets/kv2.png');
     }
 
     create(){
+        //add tank selection menu image
+        this.add.image(600, 300, 'T34').setOrigin(7,0);
+        this.add.image(600, 300, 'SU85').setOrigin(5,0);
+        this.add.image(600, 300, 'kv2').setOrigin(3,0);
         // menu display
         let menuConfig = {
             fontFamily: 'Courier',
@@ -27,14 +34,16 @@ class TankSelection extends Phaser.Scene{
         let centerY = game.config.height/2;
         let textSpacer = 100;
         
-        this.add.text(centerX, centerY - textSpacer, 'TANKSELECTION', menuConfig).setOrigin(0.5);
-        this.add.text(centerX, centerY - textSpacer/3, '1: PLAY', menuConfig).setOrigin(0.5);
-        this.add.text(centerX, centerY + textSpacer/3, '2: GO BACK TO MAIN MENU', menuConfig).setOrigin(0.5);
+        //added tank sprites for selection
+        this.add.text(centerX, centerY - textSpacer, '1: T-34', menuConfig).setOrigin(0.5);
+        this.add.text(centerX, centerY - textSpacer/3, '2: SU-85', menuConfig).setOrigin(0.5);
+        this.add.text(centerX, centerY + textSpacer/3, '3: KV-2', menuConfig).setOrigin(0.5);
+        this.add.text(centerX, centerY + textSpacer, '4: Go Back to Main Menu', menuConfig).setOrigin(0.5, 0);
+        
         
         // define keys
         keyONE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ONE);
         keyTWO = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.TWO);
-
     }
 
     update(){
