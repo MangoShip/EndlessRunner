f48792e84a7c9ce60dee35f034a76aa6a4972c76
Mingun <mingun0108@gmail.com>
2020-05-03 15:32:43 -0700

    put some texts in main.js (need to put game title)

--- src/main.js
+++ src/main.js
@@ -1,3 +1,28 @@
+/*
+    Game Title:
+    Group Name: Tekkaden
+    Group Members: Mingun Cho, Joonsu Jun, Sean Kang
+    Date Completed: 5/3/2020
+    Creative Tile Justification: 
+        In our game, we have three tanks that players can choose to play with.
+        These tanks have different damage, health, and movement speed. Each tank 
+        will provide a unique gameplay to the players, so players can choose
+        the tank that match with their preferences. The players' ability to
+        shoot to eliminate enemies is another technically interesting game
+        mechanic. Instead of dodging all the obstacles/enemies, players can 
+        now choose to shoot them to survive longer. Throughout the gameplay,
+        there will be three unique power-ups that will help the player by
+        temporarily giving small boost. 
+
+        The game also focuses on visual and audio effects. All the sprites (except
+        obstacles) have animation to give a more realistic sense of their 
+        movement. When players receive damage, the tank(player) will quickly blink
+        to give a feedback to the players that they got hit. There are also explosion
+        animation when sprite gets destroyed. When players collect the power-ups, 
+        they will also see animation according to the power-ups that they have collected.
+        There are also a wide variety of audio effects that players will hear during their 
+        gameplay.
+*/
 let config = {
     type: Phaser.CANVAS,
     width: 640,
